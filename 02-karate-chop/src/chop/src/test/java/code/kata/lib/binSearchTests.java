e43899866fd33b37a623daab7028aea64f29e427
Break Chen <break.chen@hy-tech.com.tw>
2026-01-27 16:28:59 +0800

    test: add binSearchChop tests

---
+++ 02-karate-chop/src/chop/src/test/java/code/kata/lib/binSearchTests.java
@@ -0,0 +1,32 @@
+package code.kata.lib;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class binSearchTests {
+    @Test
+    public void testChop() {
+
+        assertEquals(-1, binSearch.chop(3, new int[]{}));
+        assertEquals(-1, binSearch.chop(3, new int[]{1}));
+        assertEquals(0, binSearch.chop(1, new int[]{1}));
+        assertEquals(0, binSearch.chop(1, new int[]{1, 3, 5}));
+        assertEquals(1, binSearch.chop(3, new int[]{1, 3, 5}));
+        assertEquals(2, binSearch.chop(5, new int[]{1, 3, 5}));
+        assertEquals(-1, binSearch.chop(0, new int[]{1, 3, 5}));
+        assertEquals(-1, binSearch.chop(2, new int[]{1, 3, 5}));
+        assertEquals(-1, binSearch.chop(4, new int[]{1, 3, 5}));
+        assertEquals(-1, binSearch.chop(6, new int[]{1, 3, 5}));
+
+        assertEquals(0, binSearch.chop(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1, binSearch.chop(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2, binSearch.chop(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3, binSearch.chop(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binSearch.chop(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binSearch.chop(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binSearch.chop(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binSearch.chop(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binSearch.chop(8, new int[]{1, 3, 5, 7}));
+
+    }
+}
