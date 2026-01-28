26ee3aa55d65172cab6637eb1ec6409b2c759405
Break Chen <break.chen@hy-tech.com.tw>
2026-01-28 15:58:27 +0800

    refactor: add comment at IAccount

--- 03-banking-kata/src/java/banking-kata/src/main/java/kata/banking/lib/IAccount.java
+++ 03-banking-kata/src/java/banking-kata/src/main/java/kata/banking/lib/IAccount.java
@@ -1,7 +1,18 @@
 package kata.banking.lib;
 
 public interface IAccount {
+    /*
+     * Deposit an amount into the account.
+     */
     void deposit(int amount);
+
+    /*
+     * Withdraw an amount from the account.
+     */
     void withdraw(int amount);
+
+    /*
+     * Print the account statement.
+     */
     String printStatement();
 }
