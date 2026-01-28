d65a0be027a7ceffae608769ffb953381f52206b
Break Chen <break.chen@hy-tech.com.tw>
2026-01-28 13:57:30 +0800

    feature: implement the chop.js

--- 02-karate-chop/src/javascript/src/chop.js
+++ 02-karate-chop/src/javascript/src/chop.js
@@ -1,5 +1,17 @@
 // src/chop.js
 function chop(target, array) {
+  let left = 0;
+  let right = array.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (array[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
   return -1;
 }
 
