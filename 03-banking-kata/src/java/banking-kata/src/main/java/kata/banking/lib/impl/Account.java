9fba8a6e43a61da19da3ec49a68fbbc4e3cc68d6
Break Chen <break.chen@hy-tech.com.tw>
2026-01-28 15:24:50 +0800

    feature: implement methods for first unit test (green)

--- 03-banking-kata/src/java/banking-kata/src/main/java/kata/banking/lib/impl/Account.java
+++ 03-banking-kata/src/java/banking-kata/src/main/java/kata/banking/lib/impl/Account.java
@@ -3,23 +3,26 @@ package kata.banking.lib.impl;
 import kata.banking.lib.IAccount;
 
 public class Account implements IAccount {
+    private int balance = 0;
 
     @Override
     public void deposit(int amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+        balance += amount;
     }
 
     @Override
     public void withdraw(int amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+        balance -= amount;
     }
 
     @Override
     public String printStatement() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printStatement'");
+        StringBuilder statement = new StringBuilder();
+        statement.append("Date\tAmount\tBalance");
+        statement.append("\n");
+        statement.append("28.01.2026\t+" + balance + "\t" + balance);
+
+        return statement.toString();
     }
 
 }
