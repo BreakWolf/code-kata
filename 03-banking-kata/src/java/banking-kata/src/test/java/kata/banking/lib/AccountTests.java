f20b649c07325793b937974c92952d58e706767e
Break Chen <break.chen@hy-tech.com.tw>
2026-01-28 15:26:15 +0800

    test: testAccountDeposit3Times (red)

--- 03-banking-kata/src/java/banking-kata/src/test/java/kata/banking/lib/AccountTests.java
+++ 03-banking-kata/src/java/banking-kata/src/test/java/kata/banking/lib/AccountTests.java
@@ -17,4 +17,18 @@ public class AccountTests {
         account.deposit(500);
         assertTrue(account.printStatement().contains("+500\t500"));
     }
+
+    @Test
+    public void testAccountDeposit3Times() {
+        // Test code for deposit method
+        Account account = new Account();
+
+        account.deposit(500);
+        account.deposit(1500);
+        account.deposit(5000);
+        assertTrue(account.printStatement().contains("+500\t500"));
+        assertTrue(account.printStatement().contains("+1500\t2000"));
+        assertTrue(account.printStatement().contains("+5000\t7000"));
+    }
+
 }
